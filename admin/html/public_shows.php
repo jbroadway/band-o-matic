885e7a5dceb1ec978ae889916b8d4891baa557b4
jbroadway <johnny@johnnybroadway.com>
2011-01-13 13:21:16 -0600

    Add option to remove callback from shows

--- admin/html/public_shows.php
+++ admin/html/public_shows.php
@@ -1,5 +1,10 @@
-_br_shows_callback([
 <?php
+
+if (! $_GET['no_callback']) {
+	echo "_br_shows_callback([\n";
+} else {
+	echo "[\n";
+}
 
 $sep = "\t";
 foreach ($shows as $show) {
@@ -7,12 +12,17 @@ foreach ($shows as $show) {
 		unset ($show->ticket_link);
 	}
 	unset ($show->id);
+	$show->datetime = $show->date . ' ' . $show->time;
 	$show->time = gmdate ('g:ia', strtotime ($show->date . ' ' . $show->time));
 	$show->date = gmdate ('M j', strtotime ($show->date));
 	echo $sep . json_encode ($show);
 	$sep = ",\n\t";
 }
 
-?>
+if (! $_GET['no_callback']) {
+	echo "\n]);";
+} else {
+	echo "\n]";
+}
 
-]);
+?>
