2d514d32c6fe22ccb5a619e9a7aab43a3adc68fc
jbroadway <johnny@johnnybroadway.com>
2011-01-23 17:50:14 -0600

    Tweaks to background sizing

--- admin/html/public_css.php
+++ admin/html/public_css.php
@@ -1,24 +1,31 @@
-#index {
+.min-width-1024px #index {
 	background-image: url('<?php echo $general->bg_1024; ?>');
 	background-size: 100% auto;
+}
+
+.max-width-1024px #index {
+	background-image: url('<?php echo $general->bg_1024; ?>');
+	background-size: auto 100%;
 }
 
 .iphone-portrait #index {
 	background-image: url('<?php echo $general->bg_320x480; ?>');
+	background-size: auto;
 }
 
 .iphone-landscape #index {
 	background-image: url('<?php echo $general->bg_480x320; ?>');
+	background-size: auto;
 }
 
 .ipad-portrait #index {
 	background-image: url('<?php echo $general->bg_768; ?>');
-	background-size: auto 100%;
+	background-size: auto;
 }
 
 .ipad-landscape #index {
 	background-image: url('<?php echo $general->bg_1024; ?>');
-	background-size: 100% auto;
+	background-size: auto;
 }
 
 <?php
@@ -31,27 +38,32 @@ foreach ($albums as $album) {
 		$album->icon
 	);
 	printf (
-		"#%s {\n\tbackground-image: url('%s');\n\tbackground-size: 100%% auto;\n}\n\n",
+		".min-width-1024px #%s {\n\tbackground-image: url('%s');\n\tbackground-size: 100%% auto;\n}\n\n",
 		$id,
 		$album->bg_1024
 	);
 	printf (
-		".iphone-portrait #%s {\n\tbackground-image: url('%s');\n}\n\n",
+		".max-width-1024px #%s {\n\tbackground-image: url('%s');\n\tbackground-size: auto 100%%;\n}\n\n",
+		$id,
+		$album->bg_1024
+	);
+	printf (
+		".iphone-portrait #%s {\n\tbackground-image: url('%s');\n\tbackground-size: auto;\n}\n\n",
 		$id,
 		$album->bg_320x480
 	);
 	printf (
-		".iphone-landscape #%s {\n\tbackground-image: url('%s');\n}\n\n",
+		".iphone-landscape #%s {\n\tbackground-image: url('%s');\n\tbackground-size: auto;\n}\n\n",
 		$id,
 		$album->bg_480x320
 	);
 	printf (
-		".ipad-portrait #%s {\n\tbackground-image: url('%s');\n\tbackground-size: auto 100%%;\n}\n\n",
+		".ipad-portrait #%s {\n\tbackground-image: url('%s');\n\tbackground-size: auto;\n}\n\n",
 		$id,
 		$album->bg_768
 	);
 	printf (
-		".ipad-landscape #%s {\n\tbackground-image: url('%s');\n\tbackground-size: 100%% auto;\n}\n\n",
+		".ipad-landscape #%s {\n\tbackground-image: url('%s');\n\tbackground-size: auto;\n}\n\n",
 		$id,
 		$album->bg_1024
 	);
