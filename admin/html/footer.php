824ceccfff44b7289c9ae7fda5fc3d990ba220cf
Johnny Broadway <johnny@johnnybroadway.com>
2011-02-23 09:48:54 -0800

    Edited admin/html/footer.php via GitHub

--- admin/html/footer.php
+++ admin/html/footer.php
@@ -1,6 +1,6 @@
 </div>
 
-<p id="footer">Brought to you by <a href="http://www.johnnybroadway.com/">Johnny Broadway</a>. <a href="http://www.gnu.org/licenses/gpl.html">License (GPL)</a>. For updates and info, visit the <a href="http://github.com/jbroadway/band-o-rama">project homepage</a>.</p>
+<p id="footer">Brought to you by <a href="http://www.johnnybroadway.com/">Johnny Broadway</a>. <a href="http://www.gnu.org/licenses/gpl.html">License (GPL)</a>. For updates and info, visit the <a href="http://www.band-o-matic.org/">project homepage</a>.</p>
 </div>
 
 </body>
