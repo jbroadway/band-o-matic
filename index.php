44118713a08ab500f3f2638b961121ec7da2c450
jbroadway <johnny@johnnybroadway.com>
2011-04-07 00:05:32 -0500

    Fixed link issue in news/shows popups

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 	<title></title>
 	<meta http-equiv="X-UA-Compatible" content="chrome=1" />
 	<link rel="stylesheet" type="text/css" href="http://code.jquery.com/mobile/1.0a2/jquery.mobile-1.0a2.min.css" />
+	<link rel="stylesheet" type="text/css" href="js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="css/band-o-rama.css" />
 	<link rel="stylesheet" type="text/css" href="admin/public_css.php" />
-	<link rel="stylesheet" type="text/css" href="js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
  	<script src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
  	<script src="js/jquery.jplayer.min.js"></script>
  	<script src="js/jquery.jgfeed-min.js"></script>
